c3c72b6595b7a78fee5859b9dc37489aa2bb33e2
Jose Apicello <jose.apicello@acordeinternational.com>
2026-03-24 16:26:20 -0400

    Terminada fase de desarrollo del proyecto

--- src/main/java/com/japicello/article/articleapi/dto/ArticleResponse.java
+++ src/main/java/com/japicello/article/articleapi/dto/ArticleResponse.java
@@ -1,6 +1,7 @@
 package com.japicello.article.articleapi.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -12,9 +13,18 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class ArticleResponse {
 
+    @Schema(description = "ID unico del articulo" , example = "ART7777777")
     private String id;
+
+    @Schema(description = "Nombre del articulo" , example = "Memoria Ram")
     private String name;
+
+    @Schema(description = "Informacion adicional del articulo" , example = "Memoria ram de 5600MHz , UDIMM")
     private String description;
+
+    @Schema(description = "Coste del articulo" , example = "200.00")
     private BigDecimal price;
+
+    @Schema(description = "Modelo especifico del producto" , example = "DDR5 5600Mhz UDIMM")
     private String model;
 }
--- src/main/java/com/japicello/article/articleapi/dto/ArticleUpdateRequest.java
+++ src/main/java/com/japicello/article/articleapi/dto/ArticleUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.japicello.article.articleapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -10,9 +11,11 @@ import lombok.*;
 @AllArgsConstructor
 public class ArticleUpdateRequest {
 
+    @Schema(description = "Nueva informacion del articulo" , example ="Case SFF para estructuras minimalistas")
     @Size(max = 200)
     private String description;
 
+    @Schema(description = "Nueva informacion del modelo" , example ="JEMIP 660 Star Braker")
     @Size(max = 10)
     private String model;
 }
