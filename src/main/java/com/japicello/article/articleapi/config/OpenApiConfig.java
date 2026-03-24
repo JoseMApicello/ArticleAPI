f0127097aa45508744c316e54b459b9aff5fde17
Jose Apicello <jose.apicello@acordeinternational.com>
2026-03-24 16:30:38 -0400

    Agregado Swagger

---
+++ src/main/java/com/japicello/article/articleapi/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.japicello.article.articleapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Articles API")
+                        .version("1.0")
+                        .description("API para gestionar artículos de ejemplo"));
+    }
+}
